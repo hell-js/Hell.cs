8086f2cfc0c99010641d27be4f69c63ca06cd716
Cain Atkinson <19270622+yellowsink@users.noreply.github.com>
2021-10-11 19:28:04 +0000

    i am not smart

--- HellCs/Exceptions.cs
+++ HellCs/Exceptions.cs
@@ -5,7 +5,7 @@ namespace HellCs.Exceptions
 {
 	public class BadPerformanceException : Exception
 	{
-		private const string Msg = "Why are you trying to break Hell.cs by passing empty strings???";
+		private const string Msg = "Hell.cs disallows using multiple characters at once for performance reasons";
 
 		public override string Message => Msg;
 		internal BadPerformanceException() {}
@@ -13,7 +13,7 @@ namespace HellCs.Exceptions
 	
 	public class YouAreAnIdiotException : Exception
 	{
-		private const string Msg = "Hell.cs disallows using multiple characters at once for performance reasons";
+		private const string Msg = "Why are you trying to break Hell.cs by passing empty strings???";
 
 		public override string Message => Msg;
 		internal YouAreAnIdiotException() {}
